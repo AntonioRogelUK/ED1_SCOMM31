094bed853def2675ba41d13ca8035cb0e6f6529c
Jefte Elioenai Zamarron Pule <Jefte_Zamarron23@unikino.edu.mx>
2024-06-17 08:02:05 -0700

    Validando si se puede convertir

--- Ejercicios.Semana/Program.cs
+++ Ejercicios.Semana/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Favor de ingresar un numero del 1 al 7");
+string respuesta = Console.ReadLine();
+
+int diaEnNumero = 0;
+bool sePudoConvertir = int.TryParse(respuesta, out diaEnNumero);
+if (sePudoConvertir == true && diaEnNumero > 0 && diaEnNumero < 8)
+{
+
+}
