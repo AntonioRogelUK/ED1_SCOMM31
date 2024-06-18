b8a05666125699acc5efc6fba17a529e2f2bdf06
Jefte Elioenai Zamarron Pule <Jefte_Zamarron23@unikino.edu.mx>
2024-06-18 07:50:51 -0700

    Terminando ejercicio dia semana

--- Ejercicios.Semana/Program.cs
+++ Ejercicios.Semana/Program.cs
@@ -1,9 +1,26 @@
 ﻿Console.WriteLine("Favor de ingresar un numero del 1 al 7");
 string respuesta = Console.ReadLine();
 
-int diaEnNumero = 0;
-bool sePudoConvertir = int.TryParse(respuesta, out diaEnNumero);
-if (sePudoConvertir == true && diaEnNumero > 0 && diaEnNumero < 8)
+int diaSemana = 0;
+bool sePudoConvertir = int.TryParse(respuesta, out diaSemana);
+string[] semana = { "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo" };
+if (sePudoConvertir == true && diaSemana > 0 && diaSemana < 8)
 {
+    Console.WriteLine($"El día de la semana es: {semana[diaSemana - 1]}");
+}
+else
+{
+    MostrarMenu();
+}
 
+
+
+void MostrarMenu()
+{
+    int consecutivo = 0;
+    foreach (string dia in semana)
+    {
+        consecutivo++;
+        Console.WriteLine($"[{consecutivo}] {dia}");
+    }
 }
