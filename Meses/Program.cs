b7c54ecabcbcfeaab1be0b846ff2a0583b962fed
Jefte Elioenai Zamarron Pule <Jefte_Zamarron23@unikino.edu.mx>
2024-06-19 08:03:06 -0700

    Agregando la clase Meses

---
+++ Meses/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Meses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            CargarMeses();
+        }
+
+        static void CargarMeses()
+        {
+            Mes enero = new Mes();
+            enero.NombreDelMes = "Enero";
+            enero.DiasDelMes = 31;
+
+            Mes febrero = new Mes();
+            febrero.NombreDelMes = "Febrero";
+            febrero.DiasDelMes = 28;
+
+            Console.WriteLine(febrero.ToString());
+        }
+    }
+}
